7101fe7fd6c5d705cc3cdb69b18cc214d9a0b8c7
pof034 <pissmechyan+ghp@gmail.com>
2026-07-12 10:37:32 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
+// 右クリックを無効化
+document.addEventListener("contextmenu", function (e) {
+    e.preventDefault();
+});
 
+// ドラッグを無効化
+document.addEventListener("dragstart", function (e) {
+    e.preventDefault();
+});
